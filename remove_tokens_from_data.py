ab0dfe51249ad2a003220c7faee5b0795afb0801
Andru Cupala <andru@cupala.com>
2025-11-12 12:25:44 -0800

    Added remove_tokens_from_data.py

---
+++ remove_tokens_from_data.py
@@ -0,0 +1,16 @@
+import re
+
+def remove_tokens_from_dataset(dataset, start_tag, sep_tag, end_tag):
+    pattern = re.compile(
+        re.escape(start_tag) + r"(user|assistant)" + re.escape(sep_tag) + r"(.*?)" + re.escape(end_tag)
+    )
+    return pattern.sub(r"\2", dataset)
+
+if __name__ == "__main__":
+    with open("data.txt", "r") as f:
+        data = f.read()
+
+    restored = remove_tokens_from_dataset(data, "<START>", "<SEP>", "<END>")
+
+    with open("data.txt", "w") as f:
+        f.write(restored)
